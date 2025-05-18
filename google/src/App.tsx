f0f0e49807902a239ad627c740d6b63fb10f86a6
Jaquevic <jaquelinekuchar@gmail.com>
2025-05-18 14:51:22 -0300

    correcao Botoes

--- google/src/App.tsx
+++ google/src/App.tsx
@@ -1,20 +1,23 @@
 // src/App.tsx
 import { TextField } from '@mui/material';
 import './App.css';
+import Pesquisa from './Pesquisag/pesquisa';
+
 
 function App() {
   return (
     <div className="container">
       <h1 className="logo">Google</h1>
 
-      {/* Campo de texto do Material UI */}
       <TextField 
         label="Pesquise no Google"
         variant="outlined"
         fullWidth
         sx={{ backgroundColor: 'white', borderRadius: '30px', maxWidth: '600px', marginBottom: '20px' }}
       />
-      
+
+       <Pesquisa/>
+
     </div>
   );
 }
--- google/src/pesquisag/pesquisa.tsx
+++ google/src/pesquisag/pesquisa.tsx
@@ -1,9 +1,11 @@
-import './App.css';
 
-    function App() {
-      <div className="buttons">
-        <button>Pesquisa Google</button>
-        <button>Pesquisa Rápida</button>
-      </div>
-    }
-export default App;
+function Pesquisa() {
+  return (
+    <div className="buttons">
+      <button>Pesquisa Google</button>
+      <button>Pesquisa Rápida</button>
+    </div>
+  );
+}
+
+export default Pesquisa;
